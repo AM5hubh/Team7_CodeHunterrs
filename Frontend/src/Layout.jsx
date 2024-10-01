6bbd4d8604dbf9ddb03801f74056a8c960cfaa95
Shubham TS <ssahamate2020@gmail.com>
2024-10-01 06:27:15 +0530

    usercontext

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -3,18 +3,18 @@ import { Outlet } from "react-router-dom";
 // import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Toaster } from "react-hot-toast";
-// import { UserContextProvider } from "../context/userContext";
+import { UserContextProvider } from "../context/userContext.jsx";
 import Navbar from "./components/Navbar/Navbar";
 
 function Layout() {
   return (
     <>
-      {/* <UserContextProvider> */}
+      <UserContextProvider>
         <Navbar />
         <Outlet />
         <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
         <Footer />
-      {/* </UserContextProvider> */}
+      </UserContextProvider>
     </>
   );
 }
